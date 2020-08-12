3332ec46273ef7e2a5f2b12feead14b3f55e83a9
Ronald Silva <50335354+rohuldson@users.noreply.github.com>
2020-08-11 22:25:17 -0300

    Add files via upload

---
+++ curso-em-video/javascript/Modulo E - repeticoes/ambiente.js
@@ -0,0 +1,50 @@
+console.log('Vai começar...')
+for(var c = 1; c <=4;c++){
+    console.log(c)
+}
+
+console.log('FIM!')
+
+ 
+ 
+ 
+ 
+ /* 
+ Estrutura de repetição usando FOR
+
+
+for (var c = 1; c <= 5;c++){
+    console.log("quero pizza")
+
+}
+
+*/
+
+
+
+
+
+ /* 
+ Estrutura de repetição usando DO - WHILE
+
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++ // isso é igual C recebe valor de C + 1
+}while (c <= 10)
+
+*/
+
+
+
+
+
+/* Estrutura de repetição usando WHILE
+
+var c = 1 
+while (c <= 6 ){
+    console.log("Comi a pizza")
+    c++ // isso é igual C recebe valor de C + 1
+}
+
+*/
